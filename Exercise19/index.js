df939ace82459e6ed18e0b733d410a94232d70bd
Muhammad Shehzad <mshehzadmshehzad113440@gmail.com>
2022-10-08 15:45:31 +0500

    Update index.js

--- Exercise19/index.js
+++ Exercise19/index.js
@@ -1,2 +1,8 @@
-let no = 7;
-console.log(`The total number of people who are going to be invited: ${no}`);
+
+// importing the Exercise14/index.js file and then printing the length of that array.
+import { invitation_list } from "../Exercise14/index.js";
+
+console.log(
+  `\nThe total number of people who are going to be invited: ${invitation_list.length}`
+);
+
